f3124c612e33e5c4e19b9b4c26939f2c78dbad60
tanogrosso10 <batman@multi.com.ar>
2017-03-22 15:29:24 -0300

    Ejercicio1 parte 2
    Upload

--- Ejercicio_Parte2/Ejercicio1.php
+++ Ejercicio_Parte2/Ejercicio1.php
@@ -14,11 +14,35 @@ resultado.*/
 //para mostrar vamos a usar Var_Dump(*array*) que es el metodo que muestra pero con los errores mas detallados. 
 
 //opcion uno
-$arrayuno = array(rand(),rand(),rand(),rand(),rand())
+echo "<BR>FORMA UNO<BR>";
+$arrayuno = array(rand(),rand(),rand(),rand(),rand());
 
 var_dump($arrayuno);
 
+//opcion dos 
+
+for($i=0;$i<5;$i++)
+{$arraydos[$i]=array(rand());}
+
+echo "<BR><BR>FORMA DOS<BR>";
+var_dump($arraydos);
 
 
+//opcion tres
+echo "<BR><BR>FORMA TRES<BR>";
+$arraytres = array(rand());
+array_push($arraytres,rand(),rand(),rand(),rand());
+var_dump($arraytres);
+
+
+//opcion cuatro}
+echo "<BR><BR>FORMA CUATRO<BR>";
+$arraycuatro['cero'] = rand();
+$arraycuatro['uno'] = rand();
+$arraycuatro['dos'] = rand();
+$arraycuatro['tres'] = rand();
+$arraycuatro['cuatro'] = rand();
+
+var_dump($arraycuatro);
 
 ?>
