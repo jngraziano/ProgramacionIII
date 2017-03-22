7cc2834e6b68afe0ffa01acc61a1d5b7570cec3d
tanogrosso10 <jngraziano@gmail.com>
2017-03-21 22:09:01 -0300

    Refresh

--- Ejercicio_Parte2/Ejercicio1.php
+++ Ejercicio_Parte2/Ejercicio1.php
@@ -1,4 +1,4 @@
-<?php> /*Aplicación Nº 6 (Carga aleatoria)
+<?php /*Aplicación Nº 6 (Carga aleatoria)
 Definir un Array de 5 elementos enteros y asignar a cada uno de ellos un número (utilizar la
 función rand ). Mediante una estructura condicional, determinar si el promedio de los números
 son mayores, menores o iguales que 6. Mostrar un mensaje por pantalla informando el
@@ -9,11 +9,12 @@ resultado.*/
 //opcion dos --> for
 //opcion tres --> array_push( , )
 //opcion cuatro --> array asociativos. 
+//angular2 pagina para leer
 
 //para mostrar vamos a usar Var_Dump(*array*) que es el metodo que muestra pero con los errores mas detallados. 
 
 //opcion uno
-$arrayuno = array(rand().rand().rand().rand().rand())
+$arrayuno = array(rand(),rand(),rand(),rand(),rand())
 
 var_dump($arrayuno);
 
