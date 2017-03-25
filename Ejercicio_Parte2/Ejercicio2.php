fd5e1de4f68ed45ef63390f9e1a67cee4bcbc702
tanogrosso10 <jngraziano@gmail.com>
2017-03-25 02:05:06 -0300

    Carga nueva

---
+++ Ejercicio_Parte2/Ejercicio2.php
@@ -0,0 +1,26 @@
+<?php
+//Aplicación Nº 7 (Mostrar impares)
+//Generar una aplicación que permita cargar los primeros 10 números impares en un Array.
+//Luego imprimir (utilizando la estructura for) cada uno en una línea distinta (recordar que el
+//salto de línea en HTML es la etiqueta <br/>). Repetir la impresión de los números utilizando
+//las estructuras while y foreach.
+$cont = 0;
+for($i=0;$i<10;$i++)
+{
+    $arrayimpar=array($i);
+    
+}
+
+//for($i=0;$i<10;$i++)
+//{
+//var_dump($arrayimpar);
+//echo "<br>";
+//}
+var_dump($arrayimpar);
+
+
+
+
+
+
+?>
