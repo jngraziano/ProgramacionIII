e6a908696d85bb643010d3d70e1c6e6568cd967d
tanogrosso10 <jngraziano@gmail.com>
2017-03-28 22:01:56 -0300

    Cambio setcolor

--- Ejercicio_Parte4/Ejercicio1_App15/index.php
+++ Ejercicio_Parte4/Ejercicio1_App15/index.php
@@ -1,15 +1,17 @@
 <?php
 
-include "FiguraGeometrica.php";
-include "Rectangulo.php";
-include "Triangulo.php";
+require "FiguraGeometrica.php";
+require "Rectangulo.php";
+require "Triangulo.php";
 
 $rectangulo = new Rectangulo(2,4);
 $triangulo = new Triangulo(3,3);
 echo "RECTANGULO<br>";
+$rectangulo->SetColor("<br>Color: Granate");
 $rectangulo->ToString();
 echo "<br><br>";
 echo"TRIANGULO<br>";
+$triangulo->SetColor("<br>Color: Blanco");
 $triangulo->ToString();
 
 
