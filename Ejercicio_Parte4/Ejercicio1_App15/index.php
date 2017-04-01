e0ad7a9373250e9ff10b2d459ff190732d82ebfd
tanogrosso10 <jngraziano@gmail.com>
2017-04-01 16:56:42 -0300

    Update
    Fix App15(finalizado) y inicio TP_parte_01

--- Ejercicio_Parte4/Ejercicio1_App15/index.php
+++ Ejercicio_Parte4/Ejercicio1_App15/index.php
@@ -4,8 +4,8 @@ require "FiguraGeometrica.php";
 require "Rectangulo.php";
 require "Triangulo.php";
 
-$rectangulo = new Rectangulo(2,4);
-$triangulo = new Triangulo(3,3);
+$rectangulo = new Rectangulo(3,4);
+$triangulo = new Triangulo(5,3);
 echo "RECTANGULO<br>";
 $rectangulo->SetColor("<br>Color: Granate");
 $rectangulo->ToString();
@@ -14,4 +14,12 @@ echo"TRIANGULO<br>";
 $triangulo->SetColor("<br>Color: Blanco");
 $triangulo->ToString();
 
+echo "<br><br>";
+echo "<br><br>";
+echo "<br><br>";
+echo "<br><br>";
+echo "<br><br>";
+var_dump($rectangulo);
+echo "<br><br>";
+var_dump($triangulo);
 ?>
