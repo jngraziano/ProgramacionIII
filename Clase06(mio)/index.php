95ce7e8405af3afe7658568bb6ec68d2754f82aa
tanogrosso10 <jngraziano@gmail.com>
2017-04-25 21:39:14 -0300

    Clase06, Clase06(abm),Clase06(mio)

---
+++ Clase06(mio)/index.php
@@ -0,0 +1,32 @@
+<?php
+//Sistema a AccesoaDatos lo tenemos que llevar a todos lados porque nos permite comunicarnos con la BD
+/*En las clases llamamos a ese metodo para guardar borrar y traer. 
+Clases guarda los datos con acceso a bd y nunca va a afectar directamente a la BD
+Desde html no puedo llamar al metodo de la clase, pero si un archivo
+Entonces creo un php (Guardar.php o Nexo.php o Administrador.php o Administracion.php) que sea un administrador o nexo que llame a Clase.Guardar y ahi sin
+lo meto en el submint. Osea que reciben todas las peticiones que vienen desde el html o ajax o javascrip y lo redirige.
+En este usamos un if o un switch con los nombres.
+
+Pero si quiero agregar mas de una clase en ese archivo.
+
+Parcial:
+Primero Alta
+Listar
+borrar
+Modificar
+Guardar en archivo
+Guardar en BD
+Hay que llevar lo justo y necesario que sirva para que se pueda cambiar para que funcione con lo que pida el profesor.
+bd la sabemos hacer 
+acceso lo copiamos y pegamos
+clases es siempre lo mismo
+nexo.php o administracion.php es la logica (lo dificil)
+
+
+
+*/
+
+
+
+
+?>
