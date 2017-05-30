97f825a183ad5bd281b95bff3044137e6a143b11
jngraziano <jngraziano@gmail.com>
2017-05-30 20:54:38 -0300

    SLIM2 update

--- SLIM2/index.php
+++ SLIM2/index.php
@@ -1,5 +1,6 @@
 <?php
 require 'vendor/autoload.php';
+require 'clases/AccesoDatos.php';
 
 // Create and configure Slim app
 $config = ['settings' => [
@@ -8,22 +9,47 @@ $config = ['settings' => [
 $app = new \Slim\App($config);
 
 // Define app routes
-$app->get('/saludoget', function ($request, $response) {
-    return $response->write("Hello get");
+$app->get('/traertodos', function ($request, $response) {
+    $objetoAcceso = AccesoDatos::DameUnObjetoAcceso();
+    $consulta = $objetoAcceso->RetornarConsulta('SELECT * FROM usuario');
+    $consulta->execute();
+    $todos = $consulta->fetchAll();
+    return $response->withJson($todos);
 });
-$app->get('/saludoget2', function ($request, $response) {
-    return $response->write("Hello get");
+$app->get('/traeruno/[{id}]', function ($request, $response, $args) {
+          $objetoAcceso = AccesoDatos::DameUnObjetoAcceso();
+          $consulta = $objetoAcceso->RetornarConsulta('SELECT ID, CLAVE, MAIL, ESTADO FROM usuario WHERE id=:id');
+          $consulta->bindParam("id", $args['id']);
+           $consulta->execute();
+            $uno = $consulta->fetchAll();
+            return $response->withJson($uno);
+        });
+$app->post('/alta', function ($request, $response) {
+    return $response->write("alta.");
 });
-$app->post('/saludopost', function ($request, $response) {
-    return $response->write("Hello post.");
+$app->delete('/baja', function ($request, $response) {
+    return $response->write("delete.");
 });
-$app->get('/todo/[{id}]', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM tasks WHERE id=:id");
-        $sth->bindParam("id", $args['id']);
-        $sth->execute();
-        $todos = $sth->fetchObject();
-        return $this->response->withJson($todos);
-    });
+$app->put('/modificacion', function ($request, $response) {
+    return $response->write("modificacion.");
+});
+$app->patch('/cambiarestado', function ($request, $response) {
+    return $response->write("cambiarestado.");
+});
+$app->post('/validarusuario', function ($request, $response) {
+    return $response->write("validarusuario.");
+});
+
+
+
+
+// $app->get('/funciones/[{id}]', function ($request, $response, $args) {
+//          $sth = $this->db->prepare("SELECT * FROM tasks WHERE id=:id");
+//         $sth->bindParam("id", $args['id']);
+//         $sth->execute();
+//         $todos = $sth->fetchObject();
+//         return $this->response->withJson($todos);
+//     });
 
 // Run app
 $app->run();
